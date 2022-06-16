72334e31be371644206565be731571c0d070a131
amiteshsethi <amiteshsethi@gmail.com>
2022-06-17 00:39:11 +0530

    hnadling the error caused by [git checkout]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
-import Settings from "./Settings";
+import Settings from "./settings";
 import UserProfile from "./UserProfile";
 export { Home, Login, Signup, Settings ,UserProfile };
