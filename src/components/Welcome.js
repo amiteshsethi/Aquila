8bb07eaabcabd7df9cab8a094fbc8a64d77baf1c
amiteshsethi <amiteshsethi@gmail.com>
2022-06-16 18:18:44 +0530

    styles cahnges and background tweeks

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import styles from "../styles/welcome.module.css";
 const Welcome = () => {
   return (
     <div className={styles.welcome}>
-      <img src="https://m.media-amazon.com/images/I/61h7vvsg31L._SL1000_.jpg" />
+      <img alt="welcome-img" src="https://m.media-amazon.com/images/I/61h7vvsg31L._SL1000_.jpg" />
       <div className={styles.welcomeHeader}>WELCOME TO AQUILA</div>
       <span>connect and share with the people in your life.</span>
     </div>
